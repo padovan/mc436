a750eafe3106ccc090a5d4481b7a5372e0707eab
gut <gsscalet@gmail.com>
2009-05-31 14:39:09 -0300

    Correcting tab indent
    And fixing some spaces

--- settings.py
+++ settings.py
@@ -29,18 +29,18 @@ DEBUG = True
 TEMPLATE_DEBUG = DEBUG
 
 ADMINS = (
-	 ('Gustavo F. Padovan', 'gustavo@padovan.org'),
-	 ('Gustavo Serra Scalet', 'gut@las.ic.unicamp.br'),
+	('Gustavo F. Padovan', 'gustavo@padovan.org'),
+	('Gustavo Serra Scalet', 'gut@las.ic.unicamp.br'),
 )
 
 MANAGERS = ADMINS
 
-DATABASE_ENGINE = 'sqlite3'		   # 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
-DATABASE_NAME = 'conference.sqlite3'			 # Or path to database file if using sqlite3.
-DATABASE_USER = ''			 # Not used with sqlite3.
-DATABASE_PASSWORD = ''		 # Not used with sqlite3.
-DATABASE_HOST = ''			 # Set to empty string for localhost. Not used with sqlite3.
-DATABASE_PORT = ''			 # Set to empty string for default. Not used with sqlite3.
+DATABASE_ENGINE = 'sqlite3'  # 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
+DATABASE_NAME = 'conference.sqlite3'  # Or path to database file if using sqlite3.
+DATABASE_USER = ''  # Not used with sqlite3.
+DATABASE_PASSWORD = ''   # Not used with sqlite3.
+DATABASE_HOST = ''  # Set to empty string for localhost. Not used with sqlite3.
+DATABASE_PORT = ''  # Set to empty string for default. Not used with sqlite3.
 
 # Local time zone for this installation. Choices can be found here:
 # http://en.wikipedia.org/wiki/List_of_tz_zones_by_name
